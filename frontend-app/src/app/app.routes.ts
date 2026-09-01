88239c6ce491bf7dadb8da9abadd5f341e299a7b
Shiv Soni <sonishiv309@gmail.com>
2026-09-01 12:32:45 +0530

    added login, signup and home page

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '', redirectTo : 'home', pathMatch : 'full'
+    },
+    {
+        path : "home", loadComponent : () => import('./Features/Home/Home').then(m => m.Home)
+    },
+    {
+        path : "auth", loadChildren : () => import('./Features/Auth/Auth.routes').then(m => m.AuthRoutes)
+    },
+    {
+        path : '**', redirectTo : 'home', pathMatch : 'full'
+    }
+];
